85cfd9a3a823d33a7c0e5489f423d52582eb1f1b
Nick May <nickmay9@outlook.com>
2020-12-14 10:57:01 -1000

    question prompt and first user input

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers => console.log(answers));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
